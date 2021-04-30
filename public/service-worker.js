5276c4ef2864976a4c84c7d584097e770b8762a0
Dawan Sawyer <dawansawyer@gmail.com>
2021-04-29 21:33:32 -0400

    fixed service worker error still working on png, ive compressed once already

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,40 +5,28 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/manifest.webmanifest",
-  "./styles.css",
+  "/styles.css",
   "/index.js",
-  "/assets/images/icons/budget72.png",
-  "/assets/images/icons/budget96.png",
-  "/assets/images/icons/budget128.png",
-  "/assets/images/icons/budget144.png",
-  "/assets/images/icons/budget152.png",
-  "/assets/images/icons/budget192.png",
-  "/assets/images/icons/budget384.png",
-  "/assets/images/icons/budget512.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
-// install
-self.addEventListener("install", function (evt) {
-  // pre cache transaction data
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-  );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache')
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
 
-  // pre cache all static assets
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
-
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function (evt) {
+self.addEventListener("activate", function(evt) {
   evt.waitUntil(
-    caches.keys().then((keyList) => {
+    caches.keys().then(keyList => {
       return Promise.all(
-        keyList.map((key) => {
+        keyList.map(key => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
@@ -51,42 +39,32 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-self.addEventListener("fetch", function (evt) {
-  // code to handle requests goes here
-});
-
-// // fetch
-self.addEventListener("fetch", function (evt) {
-  // cache successful requests to the API
+self.addEventListener("fetch", (evt) => {
+  
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(evt.request)
-            .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
 
-              return response;
-            })
-            .catch((err) => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        })
-        .catch((err) => console.log(err))
+            return response;
+          })
+          .catch(err => {
+            
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
     );
 
-    return;
-  }
-  evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request).then((response) => {
+    return;}
+
+    evt.respondWith(
+      caches.match(evt.request).then((response) => {
         return response || fetch(evt.request);
-      });
-    })
-  );
-});
+      })
+    );
+  });
